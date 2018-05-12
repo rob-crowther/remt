2845d6f90c15328dc4c573b50892adbfa353b766
wrobell <wrobell@pld-linux.org>
2018-05-12 22:17:35 +0100

    - updated the project pypi classifiers

--- setup.py
+++ setup.py
@@ -42,7 +42,9 @@ reMarkable tablet tools
     classifiers=[
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Programming Language :: Python :: 3',
-        'Development Status :: 4 - Beta',
+        'Development Status :: 3 - Alpha',
+        'Topic :: Multimedia :: Graphics :: Vector-Based',
+        'Topic :: Scientific/Engineering',
     ],
     keywords='remarkable tools',
     license='GPL',
