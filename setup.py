0a3e33afe554c96c3b13dd0711f220ad08e0f529
wrobell <wrobell@pld-linux.org>
2018-05-16 20:08:52 +0100

    - center image in the project description

--- setup.py
+++ setup.py
@@ -69,6 +69,7 @@ COPYING file for details. As stated in the license, there is no warranty,
 so any usage is on your own risk.
 
 .. image:: https://gitlab.com/wrobell/remt/raw/master/examples/rm.png
+    :align: center
 """,
     classifiers=[
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
