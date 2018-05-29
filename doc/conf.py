8eba30fb19e47be7ceaf5ed9b423e92fe400c7e2
wrobell <wrobell@pld-linux.org>
2018-05-29 23:30:59 +0100

    - starting work on user documentation

---
+++ doc/conf.py
@@ -0,0 +1,25 @@
+import sphinx_rtd_theme
+
+extensions = [
+    'sphinx.ext.autodoc', 'sphinx.ext.autosummary', 'sphinx.ext.doctest',
+    'sphinx.ext.todo', 'sphinx.ext.viewcode', 'sphinx.ext.mathjax'
+]
+project = 'remt'
+source_suffix = '.rst'
+master_doc = 'index'
+
+version = release = 0.4.1
+copyright = 'remt team'
+
+epub_basename = 'remt - {}'.format(version)
+epub_author = 'remt team'
+
+todo_include_todos = True
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['static']
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_style = 'remt.css'
+
+
+# vim: sw=4:et:ai
