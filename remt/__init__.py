42031813f211b311123777a955b8c16c9e15723d
wrobell <wrobell@pld-linux.org>
2019-07-17 01:09:57 +0100

    - ver. 0.5.1

--- remt/__init__.py
+++ remt/__init__.py
@@ -20,7 +20,7 @@
 from .drawer import draw_context, draw
 from .parser import parse, empty_page
 
-__version__ = '0.5.0'
+__version__ = '0.5.1'
 
 __all__ = ['draw_context', 'draw', 'parse', 'empty_page', '__version__']
 
